602acc15163c4e9b16742996b5df398050a0fde0
erick-sudo <erickochieng766@gmail.com>
2023-03-15 11:59:23 +0300

    Passed Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  wrap_parameters format: []
+  rescue_from ActiveRecord::RecordNotFound, with: :no_record_found_response
 
+  private
+  def no_record_found_response
+    render json: {error: "#{controller_name.classify} not found"}, status: :not_found
+  end
 end
